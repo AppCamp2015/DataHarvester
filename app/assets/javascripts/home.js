e554617007c93a90556763a757946eb9099f88f9
ClaireC3 <c.crotty3@nuigalway.ie>
2015-09-18 10:44:41 +0100

    Ui changes

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -26,7 +26,39 @@ function selectCategory(id){
     newdiv.setAttribute('max', "100");
     newdiv.setAttribute('value', "");
     newLabel.innerHTML = id;
+    newLabel.setAttribute('id', id+"range-Label");
     document.getElementById('timeSearch').appendChild(newLabel);
     document.getElementById('timeSearch').appendChild(newdiv);
     document.getElementById(id).remove(); 
+
+ //   appendElementtoDropdown(id, $('#remove-filter-options'));
+
+}
+
+function removeFilter(removeItem){
+
+ console.log('slider: ', document.getElementById(removeItem+"range")); 
+ console.log('label: ', document.getElementById(removeItem+"range-Label")); 
+
+ document.getElementById(removeItem+"range").remove(); 
+ document.getElementById(removeItem+"range-Label").remove(); 
+  
+  var list = document.getElementById('remove-filter-options');
+  var item = document.getElementById(removeItem);
+  list.removeChild(item);
+}
+
+function appendElementtoDropdown(item, list){
+    //add it to the list of filters that can be removed
+    var newItem = document.createElement('li');
+
+    newItem.setAttribute('id', item);
+    newItem.setAttribute('value', item);
+    console.log('newItem Id: ', newItem.id);
+    newItem.onclick = removeFilter(newItem.id); 
+    console.log('click: ', newItem.onclick );
+    console.log('value',$('#'+item).val);
+    list.append(item);
+    console.log('list: ', list);
+    console.log('item: ', item);
 }
