2512daef4834c41cfa2151f3e257f47eecccfe10
ClaireC3 <c.crotty3@nuigalway.ie>
2015-09-18 08:41:42 +0100

    UI - will tidy now

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,32 @@
+function splunkSearch(){
+    var obj= {};
+    var objects = $('.rangeFilter');
+
+    obj['queryName'] = document.getElementById('searchName').value;
+
+
+    for(var i =0;i<objects.length; i++){
+        console.log(i);
+        var id = objects[i].getAttribute('id');
+        var value = objects[i].value;
+        
+        obj[id] = value/100;
+    };
+    console.log('object for splunk search:', obj); 
+}
+
+
+function selectCategory(id){
+    var newLabel = document.createElement('label');
+    var newdiv = document.createElement('input');
+    newdiv.setAttribute('class',"rangeFilter");
+    newdiv.setAttribute('type',"range");
+    newdiv.setAttribute('id',id + "range");
+    newdiv.setAttribute('min', "0");
+    newdiv.setAttribute('max', "100");
+    newdiv.setAttribute('value', "");
+    newLabel.innerHTML = id;
+    document.getElementById('timeSearch').appendChild(newLabel);
+    document.getElementById('timeSearch').appendChild(newdiv);
+    document.getElementById(id).remove(); 
+}
